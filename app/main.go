f36f945e2d8218b401fe88f4620cd3733b749583
Abi Liu <abiliu018@gmail.com>
2024-08-02 15:16:11 -0700

    integration tests

--- app/main.go
+++ app/main.go
@@ -5,18 +5,7 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	return
-}
-
 func main() {
-	store := &InMemoryPlayerStore{}
-	server := &PlayerServer{store: store}
+	server := &PlayerServer{store: NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
