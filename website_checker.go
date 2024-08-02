bc49b3774ee93c424dd84091be790ed45b94030f
Abi Liu <abiliu018@gmail.com>
2024-08-01 19:11:57 -0700

    concurrency

---
+++ website_checker.go
@@ -0,0 +1,31 @@
+package main
+
+import "time"
+
+type WebsiteChecker func(string) bool
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	type res struct {
+		url string
+		ok  bool
+	}
+	resChan := make(chan res, 100)
+	defer close(resChan)
+
+	for _, url := range urls {
+		go func() {
+			ok := wc(url)
+			resChan <- res{url: url, ok: ok}
+		}()
+	}
+
+	for i := 0; i < len(urls); i++ {
+		res := <-resChan
+		results[res.url] = res.ok
+	}
+
+	time.Sleep(2 * time.Second)
+
+	return results
+}
