c0b7189f8f2923ec7b67ebcbc391e9ca98ecd5fc
Abi Liu <abiliu018@gmail.com>
2024-08-01 22:45:25 -0700

    property tests

---
+++ property_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"7 gets converted to IV (can't repeat more than 3 times)", 7, "VII"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
+		{"49 gets converted to IL", 49, "XXXXIX"},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
+}
